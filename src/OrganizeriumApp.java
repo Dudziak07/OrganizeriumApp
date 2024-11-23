d67c82a1481fdadcb48c8c1a3bc9f9f86320b297
Filip Dudziec <dudziaky07@gmail.com>
2024-11-23 10:38:28 +0100

    Dodanie systemu logów

--- src/OrganizeriumApp.java
+++ src/OrganizeriumApp.java
@@ -6,8 +6,10 @@ public class OrganizeriumApp {
     public static void main(String[] args) {
         Logger.log("Uruchomienie aplikacji", "OrganizeriumApp została uruchomiona");
 
-        TaskController controller = new TaskController();
+        TaskController controller= new TaskController();
         MainMenuView mainMenu = new MainMenuView(controller);
         mainMenu.show();
     }
 }
+
+
